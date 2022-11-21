56fdc711612ee5c9b04bd02a512283dc29074476
Gilbert <djbuzzkill@gmail.com>
2022-11-20 20:47:51 -0700

    more files

---
+++ rokz/defaults.cpp
@@ -0,0 +1,71 @@
+#include "defaults.h"
+
+
+
+using namespace rokz;
+
+VkInstanceCreateInfo& rokz::Default (VkInstanceCreateInfo& create_info) {
+  return create_info;
+}
+
+VkDeviceCreateInfo& rokz::Default (VkDeviceCreateInfo& create_info) {
+  return create_info;
+}
+
+VkDeviceQueueCreateInfo& rokz::Default (VkDeviceQueueCreateInfo& create_info) {
+  return create_info;
+}
+
+VkSwapchainCreateInfoKHR& rokz::Default (VkSwapchainCreateInfoKHR& create_info) {
+  return create_info;
+}
+
+VkImageViewCreateInfo& rokz::Default (VkImageViewCreateInfo& create_info) {
+  return create_info;
+} 
+
+VkRenderPassCreateInfo& rokz::Default (VkRenderPassCreateInfo& create_info) {
+  return create_info;
+}
+
+VkGraphicsPipelineCreateInfo& rokz::Default (VkGraphicsPipelineCreateInfo& create_info) {
+  return create_info;
+}
+
+VkPipelineInputAssemblyStateCreateInfo&  rokz::Default (VkPipelineInputAssemblyStateCreateInfo& create_info) {
+  return create_info;
+}       //      ateInfo input_assembly{};
+
+VkPipelineDynamicStateCreateInfo& rokz::Default (VkPipelineDynamicStateCreateInfo& create_info) {
+  return create_info;
+} //         dynamic_state_create_info {};
+
+VkPipelineViewportStateCreateInfo& rokz::Default (VkPipelineViewportStateCreateInfo& create_info) {
+  return create_info;
+}    //    viewport_state_create_info{};
+
+VkPipelineRasterizationStateCreateInfo& rokz::Default (VkPipelineRasterizationStateCreateInfo& create_info) {
+  return create_info;
+}//  rasterizer{};
+
+VkPipelineMultisampleStateCreateInfo& rokz::Default (VkPipelineMultisampleStateCreateInfo& create_info)  {
+  return create_info;
+} //   multisampling{};
+
+VkPipelineDepthStencilStateCreateInfo& rokz::Default (VkPipelineDepthStencilStateCreateInfo& create_info)  {
+  return create_info;
+} //    pipeline_depth_stencil_create_info {};
+
+VkPipelineColorBlendStateCreateInfo& rokz::Default (VkPipelineColorBlendStateCreateInfo& create_info) {
+  return create_info;
+}   //    color_blending_create_info{};
+
+VkPipelineLayoutCreateInfo&  rokz::Default (VkPipelineLayoutCreateInfo& create_info) {
+  return create_info;
+}
+
+VkPipelineShaderStageCreateInfo& rokz::Default (VkPipelineShaderStageCreateInfo& create_info) {
+  return create_info;
+} //      vert_shader_stage_info {};
+
+
---
+++ rokz/defaults.h
@@ -0,0 +1,29 @@
+
+#ifndef ROKZ_DEFAULTS_INCLUDE
+#define ROKZ_DEFAULTS_INCLUDE
+
+#include "common.h"
+
+
+namespace rokz {
+
+  VkInstanceCreateInfo&                    Default (VkInstanceCreateInfo& create_info); // 
+  VkDeviceCreateInfo&                      Default (VkDeviceCreateInfo& create_info);
+  VkDeviceQueueCreateInfo&                 Default (VkDeviceQueueCreateInfo& create_info);    
+  VkSwapchainCreateInfoKHR&                Default (VkSwapchainCreateInfoKHR& create_info);  
+  VkImageViewCreateInfo&                   Default (VkImageViewCreateInfo& create_info);  
+  VkRenderPassCreateInfo&                  Default (VkRenderPassCreateInfo& create_info); 
+  VkGraphicsPipelineCreateInfo&            Default (VkGraphicsPipelineCreateInfo& create_info); 
+  VkPipelineInputAssemblyStateCreateInfo&  Default (VkPipelineInputAssemblyStateCreateInfo& create_info);       //      ateInfo input_assembly{};
+  VkPipelineDynamicStateCreateInfo&        Default (VkPipelineDynamicStateCreateInfo& create_info); //         dynamic_state_create_info {};
+  VkPipelineViewportStateCreateInfo&       Default (VkPipelineViewportStateCreateInfo& create_info);    //    viewport_state_create_info{};
+  VkPipelineRasterizationStateCreateInfo&  Default (VkPipelineRasterizationStateCreateInfo& create_info);//  rasterizer{};
+  VkPipelineMultisampleStateCreateInfo&    Default (VkPipelineMultisampleStateCreateInfo& create_info);  //   multisampling{};
+  VkPipelineDepthStencilStateCreateInfo&   Default (VkPipelineDepthStencilStateCreateInfo& create_info);  //    pipeline_depth_stencil_create_info {};
+  VkPipelineColorBlendStateCreateInfo&     Default (VkPipelineColorBlendStateCreateInfo& create_info);    //    color_blending_create_info{};
+  VkPipelineLayoutCreateInfo&              Default (VkPipelineLayoutCreateInfo& create_info);
+  VkPipelineShaderStageCreateInfo&         Default (VkPipelineShaderStageCreateInfo& create_info); //      vert_shader_stage_info {};
+
+  
+}                                                                                 
+#endif
