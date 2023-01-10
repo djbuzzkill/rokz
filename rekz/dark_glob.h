c13d034fb45cd580f628805277d2fe692372ef65
Gilbert <djbuzzkill@gmail.com>
2023-01-09 20:07:28 -0700

    glob header

---
+++ rekz/dark_glob.h
@@ -0,0 +1,98 @@
+
+#ifndef DARKROOT_GLOB_INCLUDE
+#define DARKROOT_GLOB_INCLUDE
+
+
+
+namespace darkroot {
+
+  //    3.14159265f;
+  // --------------------------------------------------------------------
+  // Geometry
+  // --------------------------------------------------------------------
+  template<typename VTy, typename IndTy>
+    struct GeometryData {
+
+      typedef  VTy         VertexType; 
+      typedef  IndTy       IndexType; 
+
+      enum { VertexSize = sizeof(VTy) }; 
+      enum { IndexSize  = sizeof(IndTy) }; 
+  
+      std::vector<VTy> verts;
+      std::vector<IndTy> indices;
+    }; 
+
+
+  // --------------------------------------------------------------------
+  template<typename VTy> 
+    using TriMesh = GeometryData<VTy, uint16_t>; 
+
+  // --------------------------------------------------------------------
+  // struct Renderable 
+  // --------------------------------------------------------------------
+  struct Renderable {
+
+    virtual auto SetupRS (VkCommandBuffer commandbuffer) -> int = 0;
+    virtual auto Draw    (VkCommandBuffer commandbuffer) -> void = 0;
+    
+    virtual auto AllocRes (VmaAllocator& alloc) -> int = 0; 
+    virtual auto FreeRes  (VmaAllocator& alloc) -> int = 0; 
+
+  protected:
+    Renderable () = default; 
+  
+  }; 
+
+  // --------------------------------------------------------------------
+  // 
+  // --------------------------------------------------------------------
+  namespace HalfEdge {
+
+    constexpr int32_t NullIndex = -1; 
+    typedef   int32_t Index; 
+
+    // --------------------------------------------------------------------
+    struct Face {
+      Index edgei;
+    }; 
+        
+    // --------------------------------------------------------------------
+    struct Edge {
+      Index verti; 
+      Index pairi; 
+      Index facei; 
+      Index nexti; 
+    }; 
+      
+    // --------------------------------------------------------------------
+    typedef std::vector<Edge> Edges;
+    typedef std::vector<Face> Faces;
+
+    // --------------------------------------------------------------------
+    struct BRep {
+      // edgei[i] -> geometry.verts[i] 
+      std::vector<Index> edgei;
+      // edge store
+      Edges edges;
+      // face store
+      Faces faces;
+    }; 
+
+  } // HalfEdge
+
+
+  // --------------------------------------------------------------------
+  //
+  // --------------------------------------------------------------------
+  struct DarkrootVert {
+    glm::vec3 pos; 
+    glm::vec3 nrm; 
+    glm::vec3 col; 
+    glm::vec2 txc0; 
+  };
+
+  
+}
+
+#endif
