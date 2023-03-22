ee7a95c98f6dead017c77b62c46042a3da173e49
Gilbert <djbuzzkill@gmail.com>
2023-03-22 17:24:57 -0600

    mebe useful

---
+++ rokz/ut_offset.h
@@ -0,0 +1,61 @@
+
+#ifndef ROKZ_UT_OFFSET_INCLUDE
+#define ROKZ_UT_OFFSET_INCLUDE
+
+#include "common.h"
+#include "shared_types.h"
+
+
+namespace rokz { 
+  namespace ut { 
+
+    // ---------------------------------------------------------------------------------------
+    template<int N> inline size_t
+      offset_at (const size_t sizez[N], size_t index) {
+
+      if (index >= N)
+        return 0;
+
+      size_t acc = 0;
+
+      for (size_t i = 0; i < index; ++i)
+        acc += sizez[i];
+
+      return acc;
+    
+    }
+  
+    template<size_t N> inline size_t
+      offset_at (const std::array<size_t, N>& sizez, size_t index) {
+
+      if (index >= N)
+        return 0;
+
+      size_t acc = 0;
+
+      for (size_t i = 0; i < index; ++i)
+        acc += sizez[i];
+
+      return acc;
+
+    }
+
+  
+    inline size_t
+      offset_at (const Vec<size_t>& sizez, size_t index) {
+
+      if (index >= sizez.size ())
+        return 0;
+
+      size_t acc = 0;
+
+      for (size_t i = 0; i < index; ++i)
+        acc += sizez[i];
+
+      return acc;
+    }
+
+
+  }} // rokz::ut
+
+#endif
