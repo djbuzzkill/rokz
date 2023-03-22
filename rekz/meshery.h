8ac56b4b51983c6eec3c17f6b3256136e72e9c24
Gilbert <djbuzzkill@gmail.com>
2023-03-21 22:09:03 -0600

    this is used

---
+++ rekz/meshery.h
@@ -0,0 +1,65 @@
+//
+#ifndef REKZ_MESHERY_INCLUDE
+#define REKZ_MESHERY_INCLUDE
+
+#include "rokz/rokz.h"
+
+namespace rekz {
+
+  using namespace rokz;
+
+  namespace geom {
+    // ----------------------------------------------------------------------------------------------
+    //                           
+    // ----------------------------------------------------------------------------------------------
+    template<typename VTy, typename IndTy>
+    struct GeometryData {
+
+      typedef  VTy         VertexType; 
+      typedef  IndTy       IndexType; 
+
+      enum { VertexSize = sizeof(VTy) }; 
+      enum { IndexSize  = sizeof(IndTy) }; 
+  
+      Vec<VTy>   verts;
+      Vec<IndTy> indices;
+    }; 
+
+
+
+    // ----------------------------------------------------------------------------------------------
+    //                           
+    // ----------------------------------------------------------------------------------------------
+    template<typename VTy, typename IndTy> inline size_t
+    ComputeVertexSize (const rekz::geom::GeometryData<VTy, IndTy>& geom) {
+
+      return geom.verts.size () * rekz::geom::GeometryData<VTy, IndTy>::VertexSize ; 
+    }
+    // ----------------------------------------------------------------------------------------------
+    //                           
+    // ----------------------------------------------------------------------------------------------
+    template<typename VTy, typename IndTy> inline size_t
+    ComputeIndexSize (const rekz::geom::GeometryData<VTy, IndTy>& geom) {
+
+      return geom.indices.size () * rekz::geom::GeometryData<VTy, IndTy>::IndexSize; 
+    }
+
+    // ----------------------------------------------------------------------------------------------
+    //                           
+    // ----------------------------------------------------------------------------------------------
+    template<typename VTy, typename IndTy> inline size_t
+    ComputeTotalSize (const rekz::geom::GeometryData<VTy, IndTy>& geom) {
+      return ComputeIndexSize (geom) + ComputeVertexSize (geom); 
+    }
+    
+    // ----------------------------------------------------------------------------------------------
+    //                           
+    // ----------------------------------------------------------------------------------------------
+    template<typename VTy> 
+    using TriMesh = GeometryData<VTy, uint16_t>; 
+  
+
+  }}
+
+
+#endif
