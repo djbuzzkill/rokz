8251dc6521940a9944215840cbfadd0a7d4c77ec
Gilbert <djbuzzkill@gmail.com>
2023-04-02 16:45:55 -0600

    descriptor structs separate file

---
+++ rokz/shared_descriptor.h
@@ -0,0 +1,34 @@
+#ifndef ROKZ_SHARED_DESCRIPTOR_INCLUDE
+#define ROKZ_SHARED_DESCRIPTOR_INCLUDE
+
+#include "common.h"
+#include "shared_types.h"
+
+
+namespace rokz { namespace descriptor {
+    
+    //-------------------------------------------------------------------------------------
+  //                
+  //-------------------------------------------------------------------------------------
+  struct MVPTransform {
+    glm::mat4 model;
+    glm::mat4 view;
+    glm::mat4 proj;
+  };
+
+  // ----------------------------------------------------------------------------------------------
+  //                                    
+  // ----------------------------------------------------------------------------------------------
+  struct GridState {
+    glm::vec4 xcolor;
+    glm::vec4 ycolor;
+    glm::vec4 zcolor;
+  };
+  //sizeof (rokz::MVPTransform);
+
+
+    
+  }}
+
+
+#endif
